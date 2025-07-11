80f08ef8da547bcc9d33ecb3a92236ab667c21bf
hashierholmes <hashierholmes.ph@gmail.com>
2025-07-12 06:54:00 +0800

    update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ const path = require('path');
 const config = {
   PORT: process.env.PORT || 3000,
   FACEBOOK_VERIFY_TOKEN: process.env.FACEBOOK_VERIFY_TOKEN || 'hashia',
-  FACEBOOK_PAGE_ACCESS_TOKEN: process.env.FACEBOOK_PAGE_ACCESS_TOKEN || "", // Your actual token here
+  FACEBOOK_PAGE_ACCESS_TOKEN: process.env.FACEBOOK_PAGE_ACCESS_TOKEN || "EAAOPWwZCtXZBQBPNXsI9eKaxJepzeCZCYcfYNxe6ZBhaZBu6xto9EJmVM7jvFohZBEWifbbAXdew61vnWsSkBL6dg9nniSZCjiOHGZAZCWqsfTeGX5OBz4VCTdYDUIj6WBWSdrddkg7qAmosQEmYu3pQRqGngVB2IpSrHBL4AkPYphSAhgS0VPkrYanHEywkOE9ZBZB44ZAQhwZDZD", // Your actual token here
   GEMINI_API_KEY: process.env.GEMINI_API_KEY,
   GRAPH_API_URL: 'https://graph.facebook.com/v22.0'
 };
